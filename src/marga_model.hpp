3424577523c9529746ce3266980e50a35116141c
Vlad Negnevitsky <email>
2023-12-22 18:10:16 +0000

    Start addition of trigger command

--- src/marga_model.hpp
+++ src/marga_model.hpp
@@ -8,23 +8,25 @@ static const unsigned CSV_VERSION_MAJOR = 0, CSV_VERSION_MINOR = 3;
 
 class marga_model {
 public:
-	vluint64_t MAX_SIM_TIME;
-	Vmarga_model *vmm;
-	VerilatedFstC *tfp;
-	marga_csv *csv;
-	bool _fst_output = false, _csv_output = false;
+    vluint64_t MAX_SIM_TIME;
+    Vmarga_model *vmm;
+    VerilatedFstC *tfp;
+    marga_csv *csv;
+    bool _fst_output = false, _csv_output = false;
+    bool _trig = false;
+    unsigned _trig_time = 1000;
 
-	marga_model(int argc, char *argv[]);
-	~marga_model();
+    marga_model(int argc, char *argv[]);
+    ~marga_model();
 
-	// If system's finished, return -1; otherwise 0
-	int tick();
+    // If system's finished, return -1; otherwise 0
+    int tick();
 
-	// Reads and writes must be relative to marga's memory space
-	// (i.e. slave reg 0 has address 0x0, slave reg 1 has address
-	// 0x4, etc
-	uint32_t rd32(uint32_t mar_addr);
-	void wr32(uint32_t mar_addr, uint32_t data);
+    // Reads and writes must be relative to marga's memory space
+    // (i.e. slave reg 0 has address 0x0, slave reg 1 has address
+    // 0x4, etc
+    uint32_t rd32(uint32_t mar_addr);
+    void wr32(uint32_t mar_addr, uint32_t data);
 };
 
 #endif
